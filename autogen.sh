55005eb0c1b748ca59bded4b39a25deb2063619d
Sean Davis <smd.seandavis@gmail.com>
2018-10-13 08:20:49 -0400

    Replace autogen.sh with the standard xdt-autogen one, drop po/ChangeLog

--- autogen.sh
+++ autogen.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+#
+# Copyright (c) 2002-2009 The Xfce development team. All rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Written for Xfce by Benedikt Meurer <benny@xfce.org>
+#                 and Brian Tarricone <brian@tarricone.org>.
+#
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-PKG_NAME="xfce4-screensaver"
-
-(test -f $srcdir/configure.ac) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
+(type xdt-autogen) >/dev/null 2>&1 || {
+  cat >&2 <<EOF
+autogen.sh: You don't seem to have the Xfce development tools (at least
+            version $XDT_REQURED_VERSION) installed on your system, which
+            are required to build this software.
+            Please install the xfce4-dev-tools package first; it is available
+            from http://www.xfce.org/.
+EOF
+  exit 1
 }
 
-which xdt-autogen || {
-    echo "You need to install xdt-autogen from the Xfce Git"
-    exit 1
-}
+test -d m4 || mkdir m4
 
-REQUIRED_AUTOMAKE_VERSION=1.9
-XFCE_DATADIR="$xfce_datadir"
+XDT_AUTOGEN_REQUIRED_VERSION="4.7.2" exec xdt-autogen "$@"
 
-. xdt-autogen
-
+# vi:set ts=2 sw=2 et ai:
