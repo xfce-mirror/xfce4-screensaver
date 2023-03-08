xfce-mirror/xfce4-screensaver

commit 7aeced1f6fb39fd8887fc441b4ff491ba5bfcf35

Fix Xfconf memory management
xfconf_channel_get() is transfer none contrary to what the doc says, and
each call of xfconf_init() must be followed by a call of
xfconf_shutdown().

In particular, fixes an unref-critical when closing the prefs dialog.

src/gs-prefs.c
@@ -618,16 +618,6 @@ gs_prefs_finalize (GObject *object) {
 
     g_return_if_fail (prefs->priv != NULL);
 
-    if (prefs->priv->channel) {
-        g_object_unref (prefs->priv->channel);
-        prefs->priv->channel = NULL;
-    }
-
-    if (prefs->priv->xfpm_channel) {
-        g_object_unref (prefs->priv->xfpm_channel);
-        prefs->priv->xfpm_channel = NULL;
-    }
-
     if (prefs->themes) {
         g_slist_free_full (prefs->themes, g_free);
     }

src/xfce4-screensaver-preferences.c
@@ -2136,9 +2136,6 @@ configure_capplet (void) {
 
 static void
 finalize_capplet (void) {
-    if (screensaver_channel)
-        g_object_unref (screensaver_channel);
-
     if (active_theme)
         g_free (active_theme);
 }
@@ -2229,6 +2226,8 @@ main (int    argc,
 
     if (job)
         g_object_unref (job);
+
+    xfconf_shutdown ();
 
     return 0;
 }

src/gs-lock-plug.c
@@ -1658,9 +1658,6 @@ gs_lock_plug_finalize (GObject *object) {
     g_free (plug->priv->prefs);
     plug->priv->prefs = NULL;
 
-    g_free (plug->priv->channel);
-    plug->priv->channel = NULL;
-
     remove_response_idle (plug);
     remove_cancel_timeout (plug);
     remove_datetime_timeout (plug);

src/test-window.c
@@ -164,6 +164,7 @@ main (int    argc,
     g_object_unref (grab);
 
     gs_debug_shutdown ();
+    xfconf_shutdown ();
 
     return 0;
 }

src/xfce4-screensaver-dialog.c
@@ -594,6 +594,7 @@ int main(int    argc,
 
     gs_profile_end(NULL);
     gs_debug_shutdown();
+    xfconf_shutdown ();
 
     return 0;
 }

src/xfce4-screensaver.c
@@ -114,6 +114,7 @@ int main(int    argc,
     gs_debug("xfce4-screensaver finished");
 
     gs_debug_shutdown();
+    xfconf_shutdown ();
 
     return 0;
 }

commit c475190655800e207ccc283ef6584a67e18da8f7

Complete previous commit
Fixes: 7aeced1f6fb39fd8887fc441b4ff491ba5bfcf35

src/gs-prefs.c
@@ -625,6 +625,9 @@ gs_prefs_finalize (GObject *object) {
     g_free (prefs->logout_command);
     g_free (prefs->keyboard_command);
 
+    g_signal_handlers_disconnect_by_func (prefs->priv->channel, key_changed_cb, prefs);
+    g_signal_handlers_disconnect_by_func (prefs->priv->xfpm_channel, key_changed_cb, prefs);
+
     G_OBJECT_CLASS (gs_prefs_parent_class)->finalize (object);
 }
 

src/xfce4-screensaver-preferences.c
@@ -2136,6 +2136,12 @@ configure_capplet (void) {
 
 static void
 finalize_capplet (void) {
+    if (screensaver_channel)
+      g_signal_handlers_disconnect_by_func (screensaver_channel, key_changed_cb, NULL);
+
+    if (xfpm_channel)
+      g_signal_handlers_disconnect_by_func (xfpm_channel, key_changed_cb, NULL);
+
     if (active_theme)
         g_free (active_theme);
 }