f253c9f75292f7e98e71a73125745b48a4e66e67
Guido Falsi <mad@madpilot.net>
2023-03-18 14:17:46 +0100

    Fix build on FreeBSD
    On FreeBSD environ needs to be defined in the same source file where it is used.
    
    Linux provides a nonstandard extention to avoid needing the definition.
    
    This patch adds back code which was removed in commit 447b31948f75d133cd33e3ff3620e92d372e68ef but with #ifdef __FreeBSD__ since it is needed only there.
    
    Ref: https://man7.org/linux/man-pages/man7/environ.7.html
         https://man.freebsd.org/cgi/man.cgi?query=environ&apropos=0&sektion=0&manpath=FreeBSD+13.1-RELEASE+and+Ports&arch=default&format=html

--- src/xfce4-screensaver-preferences.c
+++ src/xfce4-screensaver-preferences.c
@@ -1700,6 +1700,10 @@ setup_for_lid_switch (void) {
     }
 }
 
+#ifdef __FreeBSD__
+extern char **environ;
+#endif
+
 static gchar **
 spawn_make_environment_for_display (GdkDisplay  *display,
                                     gchar      **envp) {
