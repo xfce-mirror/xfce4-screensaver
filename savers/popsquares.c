cd0574b279f7233ad3da547b71c9a31996d942de
Gaël Bonithon <gael@xfce.org>
2023-12-30 18:35:12 +0100

    Use GtkSocket/GtkPlug instead of GtkDrawingArea/GSThemeWindow

--- savers/popsquares.c
+++ savers/popsquares.c
@@ -24,11 +24,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <gtk/gtk.h>
+#include <gtk/gtkx.h>
 
 #include <libxfce4util/libxfce4util.h>
 
-#include "gs-theme-window.h"
 #include "gs-theme-engine.h"
 #include "gste-popsquares.h"
 
@@ -50,7 +49,8 @@ main (int    argc,
         exit (1);
     }
 
-    window = gs_theme_window_new ();
+    window = gtk_plug_new (strtoul (g_getenv ("XSCREENSAVER_WINDOW"), NULL, 0));
+    gtk_widget_set_app_paintable (window, TRUE);
     g_signal_connect (G_OBJECT (window), "destroy",
                       G_CALLBACK (gtk_main_quit), NULL);
 
