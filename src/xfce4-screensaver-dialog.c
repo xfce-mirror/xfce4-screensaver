e7fe1b5a02d49b1ae19ab27c4db6707b16de0db2
Gaël Bonithon <gael@xfce.org>
2024-02-03 10:49:54 +0100

    lock-plug: Derive from GObject and make GtkPlug a class member
    In preparation for using WleGtkPlug on Wayland.

--- src/xfce4-screensaver-dialog.c
+++ src/xfce4-screensaver-dialog.c
@@ -33,6 +33,7 @@
 #include <unistd.h>
 
 #include <gtk/gtk.h>
+#include <gtk/gtkx.h>
 
 #include <libxfce4util/libxfce4util.h>
 #include <xfconf/xfconf.h>
@@ -233,7 +234,7 @@ static gboolean auth_message_handler(GSAuthMessageStyle   style,
     gs_profile_start(NULL);
     gs_debug("Got message style %d: '%s'", style, msg);
 
-    gtk_widget_show(GTK_WIDGET(plug));
+    gtk_widget_show(gs_lock_plug_get_widget(plug));
     gs_lock_plug_set_ready(plug);
 
     ret = TRUE;
@@ -377,35 +378,37 @@ static void show_cb(GtkWidget *widget,
 }
 
 static gboolean popup_dialog_idle(gpointer user_data) {
-    GtkWidget      *widget;
+    GSLockPlug     *plug;
+    GtkWidget      *plug_widget;
     GtkCssProvider *css_provider;
 
     gs_profile_start(NULL);
 
-    widget = gs_lock_plug_new();
+    plug = gs_lock_plug_new();
+    plug_widget = gs_lock_plug_get_widget(plug);
 
     if (enable_logout) {
-        g_object_set(widget, "logout-enabled", TRUE, NULL);
+        g_object_set(plug, "logout-enabled", TRUE, NULL);
     }
 
     if (logout_command) {
-        g_object_set(widget, "logout-command", logout_command, NULL);
+        g_object_set(plug, "logout-command", logout_command, NULL);
     }
 
     if (enable_switch) {
-        g_object_set(widget, "switch-enabled", TRUE, NULL);
+        g_object_set(plug, "switch-enabled", TRUE, NULL);
     }
 
     if (status_message) {
-        g_object_set(widget, "status-message", status_message, NULL);
+        g_object_set(plug, "status-message", status_message, NULL);
     }
 
-    g_object_set(widget, "monitor-index", monitor_index, NULL);
+    g_object_set(plug, "monitor-index", monitor_index, NULL);
 
-    gtk_widget_set_size_request(widget, dialog_width, dialog_height);
+    gtk_widget_set_size_request(plug_widget, dialog_width, dialog_height);
 
-    g_signal_connect(GS_LOCK_PLUG(widget), "response", G_CALLBACK(response_cb), NULL);
-    g_signal_connect(widget, "show", G_CALLBACK(show_cb), NULL);
+    g_signal_connect(plug, "response", G_CALLBACK(response_cb), NULL);
+    g_signal_connect(plug_widget, "show", G_CALLBACK(show_cb), NULL);
 
     css_provider = gtk_css_provider_new ();
     gtk_css_provider_load_from_data (css_provider,
@@ -415,10 +418,10 @@ static gboolean popup_dialog_idle(gpointer user_data) {
     gtk_style_context_add_provider_for_screen (gdk_screen_get_default (), GTK_STYLE_PROVIDER (css_provider),
                                                GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 
-    gtk_widget_realize(widget);
-    gtk_widget_show(widget);
+    gtk_widget_realize(plug_widget);
+    gtk_widget_show(plug_widget);
 
-    g_idle_add(auth_check_idle, widget);
+    g_idle_add(auth_check_idle, plug);
 
     gs_profile_end(NULL);
 
