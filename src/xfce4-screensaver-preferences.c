xfce-mirror/xfce4-screensaver

commit 8e52df4283e9832322a872f49651c757c0a85943

build: Switch from intltool to gettext
See https://gitlab.xfce.org/xfce/xfce4-dev-tools/-/issues/41.

po/POTFILES.in
@@ -14,9 +14,9 @@ src/gs-lock-plug.c
 src/xfce4-screensaver-command.c
 src/xfce4-screensaver-configure
 src/xfce4-screensaver-dialog.c
-[type: gettext/glade]src/xfce4-screensaver-dialog.ui
+src/xfce4-screensaver-dialog.ui
 src/xfce4-screensaver-preferences.c
-[type: gettext/glade]src/xfce4-screensaver-preferences.ui
+src/xfce4-screensaver-preferences.ui
 src/xfce4-screensaver.c
 src/xfce4-screensaver.desktop.in.in
 src/xfcekbd-indicator.c

src/Makefile.am
@@ -56,7 +56,9 @@ noinst_PROGRAMS = \
 desktopdir = $(sysconfdir)/xdg/autostart
 desktop_in_files = xfce4-screensaver.desktop.in
 desktop_DATA = $(desktop_in_files:.desktop.in=.desktop)
-@INTLTOOL_DESKTOP_RULE@
+
+xfce4-screensaver.desktop: xfce4-screensaver.desktop.in
+	$(AM_V_GEN) $(MSGFMT) --desktop --template $< -d $(top_srcdir)/po -o $@
 
 xfce4_screensaver_command_SOURCES = \
 	xfce4-screensaver-command.c  \

Makefile.am
@@ -9,9 +9,9 @@ DISTCHECK_CONFIGURE_FLAGS = \
 SUBDIRS = \
 	src      \
 	savers   \
-	po       \
 	data     \
 	doc      \
+	po       \
 	$(NULL)
 
 EXTRA_DIST = \
@@ -21,25 +21,12 @@ EXTRA_DIST = \
 	COPYING.LGPL         \
 	AUTHORS              \
 	INSTALL              \
-	README.md            \
 	ChangeLog            \
 	NEWS                 \
-	intltool-extract.in  \
-	intltool-merge.in    \
-	intltool-update.in   \
-	$(NULL)
-
-DISTCLEANFILES = \
-	intltool-extract            \
-	intltool-merge              \
-	intltool-update             \
-	./po/.intltool-merge-cache  \
-	./po/.intltool-merge-cache.lock \
 	$(NULL)
 
 MAINTAINERCLEANFILES = \
 	*~             \
-	intltool-*.in  \
 	compile        \
 	configure      \
 	INSTALL        \

data/Makefile.am
@@ -10,13 +10,15 @@ man_MANS = \
 	xfce4-screensaver-preferences.1  \
 	$(NULL)
 
-@INTLTOOL_DESKTOP_RULE@
+xfce4-screensaver-preferences.desktop: xfce4-screensaver-preferences.desktop.in
+	$(AM_V_GEN)$(MSGFMT) --desktop --template $< -d $(top_srcdir)/po -o $@
 
 desktopdir = $(datadir)/applications
 desktop_in_files = xfce4-screensaver-preferences.desktop.in
 desktop_DATA = $(desktop_in_files:.desktop.in=.desktop)
 
-@INTLTOOL_DIRECTORY_RULE@
+xfce4-screensaver.directory: xfce4-screensaver.directory.in
+	$(AM_V_GEN)$(MSGFMT) --desktop --template $< -d $(top_srcdir)/po -o $@
 
 directorydir = $(datadir)/desktop-directories
 directory_in_files = \
@@ -31,8 +33,6 @@ menu_DATA = \
 
 dbussessionservicedir = $(DBUS_SESSION_SERVICE_DIR)
 dbussessionservice_DATA = org.xfce.ScreenSaver.service
-
-@INTLTOOL_XML_NOMERGE_RULE@
 
 EXTRA_DIST = \
 	$(desktop_in_files)              \

savers/Makefile.am
@@ -24,7 +24,8 @@ xfce-personal-slideshow.desktop.in: xfce-personal-slideshow.desktop.in.in Makefi
 xfce-popsquares.desktop.in: xfce-popsquares.desktop.in.in Makefile
 	$(edit) $< >$@
 
-@INTLTOOL_DESKTOP_RULE@
+%.desktop: %.desktop.in
+	$(AM_V_GEN) $(MSGFMT) --desktop --template $< -d $(top_srcdir)/po -o $@
 
 edit = sed \
 	-e 's|@FLOATERS_XFCE_IMAGE[@]|$(FLOATERS_XFCE_IMAGE)|g'  \

.gitignore
@@ -19,7 +19,6 @@ xfce4-screensaver-*/
 *.pot
 *.tgz
 *.xz
-*intltool*
 *marshal.c
 *marshal.h
 *~
@@ -27,6 +26,7 @@ config.*
 stamp-*
 
 # File Match
+ABOUT-NLS
 ChangeLog
 compile
 configure
@@ -47,6 +47,15 @@ data/xfce4-screensaver.pc
 data/xfce4-screensavers.menu
 doc/xfce4-screensaver.xml
 po/Makefile.in.in
+po/Makevars.template
+po/Rules-quot
+po/boldquot.sed
+po/en@boldquot.header
+po/en@quot.header
+po/insert-header.sin
+po/quot.sed
+po/remove-potcdate.sed
+po/remove-potcdate.sin
 savers/*.desktop.in
 savers/floaters
 savers/popsquares

configure.ac
@@ -23,7 +23,6 @@ dnl Silence warning: `ar: 'u' modifier ignored since 'D' is the default`
 dnl SEE https://www.mail-archive.com/automake-patches@gnu.org/msg07705.html
 AC_SUBST(AR_FLAGS, [cr])
 
-IT_PROG_INTLTOOL([0.50.1])
 AC_PROG_CC
 AM_PROG_CC_C_O
 AC_CANONICAL_HOST
@@ -36,9 +35,12 @@ AC_SUBST(VERSION)
 # Save flags to aclocal
 ACLOCAL="$ACLOCAL $ACLOCAL_FLAGS"
 
-GETTEXT_PACKAGE=xfce4-screensaver
-AC_SUBST(GETTEXT_PACKAGE)
-AC_DEFINE_UNQUOTED(GETTEXT_PACKAGE, "$GETTEXT_PACKAGE", [Name of default gettext domain])
+GETTEXT_PACKAGE="$PACKAGE"
+AC_SUBST([GETTEXT_PACKAGE])
+AC_DEFINE_UNQUOTED([GETTEXT_PACKAGE], ["$GETTEXT_PACKAGE"], [Name of default gettext domain])
+
+AM_GNU_GETTEXT([external])
+AM_GNU_GETTEXT_VERSION([0.19.8])
 
 dnl
 dnl Enable gsettings schema macros

data/xfce4-screensaver-preferences.desktop.in
@@ -1,7 +1,7 @@
 [Desktop Entry]
-_Name=Xfce Screensaver
-_Comment=Set your screensaver preferences
-_GenericName=Screensaver
+Name=Xfce Screensaver
+Comment=Set your screensaver preferences
+GenericName=Screensaver
 Icon=org.xfce.ScreenSaver
 Exec=xfce4-screensaver-preferences
 Terminal=false

commit a88cabae54cf1fd1bf8aceb5a7157672076947bb

Add .py extension to xfce4-screensaver-configure for gettext detection
Fixes: 8e52df4283e9832322a872f49651c757c0a85943

po/POTFILES.in
@@ -12,7 +12,7 @@ src/gs-auth-pam.c
 src/gs-listener-dbus.c
 src/gs-lock-plug.c
 src/xfce4-screensaver-command.c
-src/xfce4-screensaver-configure
+src/xfce4-screensaver-configure.py
 src/xfce4-screensaver-dialog.c
 src/xfce4-screensaver-dialog.ui
 src/xfce4-screensaver-preferences.c

src/Makefile.am
@@ -41,7 +41,7 @@ bin_PROGRAMS = \
 	$(NULL)
 
 bin_SCRIPTS = \
-	xfce4-screensaver-configure    \
+	xfce4-screensaver-configure.py    \
 	$(NULL)
 
 libexec_PROGRAMS = \
@@ -274,7 +274,7 @@ EXTRA_DIST = \
 	debug-screensaver.sh              \
 	gs-marshal.list                   \
 	xfce4-screensaver.desktop.in      \
-	xfce4-screensaver-configure       \
+	xfce4-screensaver-configure.py    \
 	xfce4-screensaver-dialog.css      \
 	xfce4-screensaver-dialog.ui       \
 	xfce4-screensaver-preferences.ui  \

src/xfce4-screensaver-preferences.c
@@ -47,7 +47,7 @@
 #include "xfce4-screensaver-preferences-ui.h"
 
 #define GPM_COMMAND "xfce4-power-manager-settings"
-#define CONFIGURE_COMMAND "xfce4-screensaver-configure"
+#define CONFIGURE_COMMAND "xfce4-screensaver-configure.py"
 
 enum {
     NAME_COLUMN = 0,