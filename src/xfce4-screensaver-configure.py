xfce-mirror/xfce4-screensaver

commit 916ee6d01a9bf2cdcf840c6fe0122ad44465ecfa

configure.py: Fix unquoted command tokens
Directory names can contain e.g. spaces.

src/xfce4-screensaver-configure.py
@@ -639,7 +639,7 @@ class ConfigurationWindow(Gtk.Window):
                         except:
                             pass
                         argument = argument.replace("%", str(value))
-                    arguments.append(argument)
+                    arguments.append(GLib.shell_quote(argument))
         value = " ".join(arguments)
         self.xfconf_settings.set_string("arguments", value)
 

commit f2952cf3f01ee7aa40833a752c8da00c75eb27f5

configure.py: Quote only command line args
Fixes: 916ee6d01a9bf2cdcf840c6fe0122ad44465ecfa
Closes: #179

src/xfce4-screensaver-configure.py
@@ -638,8 +638,8 @@ class ConfigurationWindow(Gtk.Window):
                             value = int(value)
                         except:
                             pass
-                        argument = argument.replace("%", str(value))
-                    arguments.append(GLib.shell_quote(argument))
+                        argument = argument.replace("%", GLib.shell_quote(str(value)))
+                    arguments.append(argument)
         value = " ".join(arguments)
         self.xfconf_settings.set_string("arguments", value)
 