xfce-mirror/xfce4-screensaver

commit f02364b8f1ae9080d840317c396ac50b6100890d

Do not check processes longer than 15 chars
pgrep on linux ignores such requests by default.

src/gs-lock-plug.c
@@ -57,6 +57,8 @@
 #include "xfce-bg.h"
 #include "xfce-desktop-utils.h"
 
+#define PGREP_MAX_PROCESS_LEN 15
+
 #define MDM_FLEXISERVER_COMMAND "mdmflexiserver"
 #define MDM_FLEXISERVER_ARGS "--startnew Standard"
 
@@ -180,6 +182,7 @@ toggle_infobar_visibility (GSLockPlug *plug) {
 
 static gboolean
 process_is_running (const char *name) {
+    g_return_val_if_fail (strnlen (name, PGREP_MAX_PROCESS_LEN + 1) > PGREP_MAX_PROCESS_LEN, FALSE);
     gchar *command = g_strdup_printf ("pgrep %s", name);
     int rc = system (command);
     g_free (command);

commit a66f9d04f7af8005977e91744fe66036ea1f8793

lock-plug: Fix wrong conditional
Fixes: f02364b8f1ae9080d840317c396ac50b6100890d
Closes: #186

src/gs-lock-plug.c
@@ -182,7 +182,7 @@ toggle_infobar_visibility (GSLockPlug *plug) {
 
 static gboolean
 process_is_running (const char *name) {
-    g_return_val_if_fail (strnlen (name, PGREP_MAX_PROCESS_LEN + 1) > PGREP_MAX_PROCESS_LEN, FALSE);
+    g_return_val_if_fail (strnlen (name, PGREP_MAX_PROCESS_LEN + 1) < PGREP_MAX_PROCESS_LEN, FALSE);
 #ifdef __FreeBSD__
     gchar *command = g_strdup_printf ("pgrep -ax %s", name);
 #else